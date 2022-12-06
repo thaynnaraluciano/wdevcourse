2e8e9dbc6d0b964e80b9a81aa8ca3c8a808b01af
thaynnaraluciano <thaynnaraluciano@gmail.com>
2022-12-06 09:16:34 -0300

    iniciando componente e configurando exceção eslint

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
 
 import './styles/global.css';
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
